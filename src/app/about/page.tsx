0324d450d3957dfd5226d9740763162c83bc175a
owen <wavefunctionc@gmail.com>
2025-01-22 11:29:44 +0900

    페이지 SPA 링크 코드 주석 추가

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const Home: FC = () => {
+    return (
+        <main>
+            <h1>About Page</h1>
+        </main>
+    );
+};
+
+export default Home;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,30 @@
-import { FC } from "react";
+import {FC} from "react";
+import Link from "next/link";
 
 const Home: FC = () => {
     return (
         <main>
-            <img src="/logo.png" alt="A server surrounded by magic sparkles." />
+            {/* 로고 이미지 */}
+            <img src="/logo.png" alt="A server surrounded by magic sparkles."/>
+
             <h1>Welcome to this NextJS Course!</h1>
             <p>
                 🔥 Let&apos;s get started! 🔥
             </p>
+
+            {/*
+                ==== 페이지 링크 ====
+
+                1. SPA가 아님:
+                    - 페이지 로딩 시간이 있음
+                    - a 태그의 href 속성 사용
+
+                2. SPA 유지:
+                    - 클라이언트 사이드 네비게이션
+                    - Link 컴포넌트 사용
+            */}
+            {/*<p><a href="/about">About App</a></p>*/}
+            <Link href="/about">About Us</Link>
         </main>
     );
 };
