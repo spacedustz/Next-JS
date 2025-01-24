b967ee87eaf679ed2c4325fb93f3b91d5fc8ae59
owen <floralexium@gmail.com>
2025-01-25 01:26:09 +0900

    Foodies Project 기본 이미지,로고 파일들 추가

---
+++ src/app/community/page.tsx
@@ -0,0 +1,11 @@
+import {FC} from "react";
+
+const CommunityPage: FC = () => {
+    return (
+        <main>
+            <h1>Community Page</h1>
+        </main>
+    );
+};
+
+export default CommunityPage;
