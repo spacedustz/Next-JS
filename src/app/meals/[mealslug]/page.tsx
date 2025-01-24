27d038c8de6301f6ff3664885efa866a4d397ad6
owen <floralexium@gmail.com>
2025-01-25 01:48:39 +0900

    Meals 동적 라우팅 추가

---
+++ src/app/meals/[mealslug]/page.tsx
@@ -0,0 +1,11 @@
+import {FC} from "react";
+
+const MealDetailsPage: FC = () => {
+    return (
+        <main>
+            <h1>Meal Details Page</h1>
+        </main>
+    );
+};
+
+export default MealDetailsPage;
